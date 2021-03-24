5684f15882aba6d649e574a110aa7604d9a64beb
daniela2001-png <dmbmora@outlook.com>
2021-03-24 12:14:00 -0500

    [add] comments for this script

--- create_cities.py
+++ create_cities.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 from models.city import City
 
-# Ciudades
+# create cities
 city = City(city_name='Bogotá', country_name="Colombia", state="Bogotá DC")
+# save city object
 city.save()
 
 city2 = City(city_name='Medellín', country_name="Colombia", state="Antioquia")
+# save city2 object
 city2.save()

34929ef02fe9cf33fc1c562d491ba6f75749258c
daniela2001-png <dmbmora@outlook.com>
2021-03-24 12:14:27 -0500

    [add] comments for this init

--- models/__init__.py
+++ models/__init__.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 
+# import the require libraries
 from models.engine.db_storage import DBStorage
+# create a instance called storage of the class DBStorage()
 storage = DBStorage()
+# create correctly the data to save into a db
 storage.reload()

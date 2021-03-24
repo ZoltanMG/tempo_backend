f2e06a0b5b9f76dd03a096724c8ecba4e81a72bc
daniela2001-png <dmbmora@outlook.com>
2021-03-24 12:15:31 -0500

    [add] comments for this module

--- models/base_model.py
+++ models/base_model.py
@@ -3,6 +3,7 @@
 Contains class BaseModel
 """
 
+# imports the require libraries
 from datetime import datetime
 import models
 from sqlalchemy import Column, String, DateTime
@@ -10,13 +11,20 @@ from sqlalchemy.ext.declarative import declarative_base
 import uuid
 from os import getenv
 
+# define the format of the date
 time_fmt = "%Y-%m-%dT%H:%M:%S.%f"
 
+# Base is the instance of the ORM SqlAlchemy
 Base = declarative_base()
 
 
 class BaseModel:
-    """The BaseModel class from which future classes will be derived"""
+    """
+    The BaseModel class from which future classes will be derived
+        - id : type(string(60)) will be the id of each object
+        - created_at : type(datetime) will be the date of a object is create
+        - update_at : type(datetime) will be the date of a object is update
+    """
 
     id = Column(String(60), nullable=False, primary_key=True)
     created_at = Column(DateTime, nullable=False, default=datetime.utcnow)
@@ -27,9 +35,11 @@ class BaseModel:
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = self.created_at
+        # if the key is equal to class ignore and continue
         for key, value in kwargs.items():
             if key == '__class__':
                 continue
+            # give the values of each key in the kwargs dictionary
             setattr(self, key, value)
             if type(self.created_at) is str:
                 self.created_at = datetime.strptime(self.created_at, time_fmt)
@@ -45,11 +55,14 @@ class BaseModel:
         """updates the attribute 'updated_at' with the current datetime"""
         self.updated_at = datetime.now()
         models.storage.new(self)
+        # push into a db the new object
         models.storage.save()
 
     def to_dict(self, save_to_disk=False):
         """returns a dictionary containing all keys/values of the instance"""
+        # create a copy of the dictionary
         new_dict = self.__dict__.copy()
+        # give isoformat for the keys of dates
         if "created_at" in new_dict:
             new_dict["created_at"] = new_dict["created_at"].isoformat()
         if "updated_at" in new_dict:
@@ -61,8 +74,9 @@ class BaseModel:
             new_dict.pop('amenities', None)
         if 'reviews' in new_dict:
             new_dict.pop('reviews', None)
-        #new_dict["__class__"] = self.__class__.__name__
+        # if a key '_sa_instance_state' exists delete of the new_dict
         new_dict.pop('_sa_instance_state', None)
+        # delete the pwd key in the dictionary
         if not save_to_disk:
             new_dict.pop('password', None)
         return new_dict

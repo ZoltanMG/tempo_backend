4f8785282412392b46da60f4679ac23ceedfc8f1
daniela2001-png <dmbmora@outlook.com>
2021-03-24 12:15:39 -0500

    [add] comments for this module

--- models/city.py
+++ models/city.py
@@ -1,3 +1,4 @@
+# imports the require libraries
 import models
 from models.base_model import BaseModel, Base
 from os import getenv
@@ -10,10 +11,12 @@ from sqlalchemy import ForeignKey
 class City(BaseModel, Base):
     """
     Class City: create a city
-        - city_name : type String
-        - country_name : type String
+        - city_name : type String(60)
+        - country_name : type String(60)
         - id: inherits from basemodel
+        - state: type(string(60)) will be the state of the city
         - created_at : inherits from basemodel
+        - venues: relation between table Venue
         - updated_at : inherits from basemodel
     """
     __tablename__ = 'cities'

2e9af9af33de85844a7ac5bc3f8d030c3d6f8cf1
daniela2001-png <dmbmora@outlook.com>
2021-03-24 12:14:53 -0500

    [add] comments for this module

--- models/artist.py
+++ models/artist.py
@@ -1,3 +1,4 @@
+# imports the require libraries
 import models
 from models.base_model import BaseModel, Base
 from os import getenv
@@ -9,19 +10,19 @@ from sqlalchemy import ForeignKey
 
 class Artist(BaseModel, Base):
     """
-    class Organizer:
-        - username: username login
+    class Artist:
+        - artist_name: type(string(60)) contains the name of the artist
+        - organizer_id: type(string(60)) contains the id of the organizer
+        - image_name: type(string(255)) contains the image of the artist
+        - genre_artist: type(string(80)) contains the genre of the artist
+        - social:  it is a relation between the table SocialArtist
     """
     __tablename__ = 'artists'
-    artist_name = Column(String(60), nullable=False)  # , unique=True)
+    artist_name = Column(String(60), nullable=False)
     organizer_id = Column(String(60), ForeignKey(
         'organizers.id'), nullable=False)
     image_name = Column(String(255), nullable=True)
-    # social = Column(String(1000), nullable=True)
     genre_artist = Column(String(80), nullable=False)
-    # email = Column(String(60), nullable=False)  # , unique=True)
-    # shows = relationship("Show", backref="artist")
-#    show_artists = relationship("Show", backref="artist") #####
     social = relationship("SocialArtist", backref="artist",
                           cascade="all, delete, delete-orphan")
 
@@ -29,22 +30,20 @@ class Artist(BaseModel, Base):
         """initializes city"""
         super().__init__(*args, **kwargs)
 
-
     def shows(self):
         """
-        TRAER TODOS LOS SHOWS DE UN ARTISTA
+        takes all shows of an artist
         """
         from models.show import Show, ShowArtist
+        # catch the artist id that is equeal to id in the table showArtist
         showArtists = models.storage.session.query(
             ShowArtist).filter_by(artist_id=self.id).all()
-        # listArtists = models.storage.session.query(
-        #     Artist).filter_by(id=ShowArtist.artist_id).all()
-        # # listArtistsId[showArtist.artist_id] = models.storage.session.query(
-        # Artist).filter_by(id=showArtist.id).first()
         listShows = []
+        # create a list with an id show
         for sa in showArtists:
             listShows.append(sa.show_id)
         shows = []
+        # and returns a list with the shows of this artist
         for show in listShows:
             shows.append(models.storage.session.query(
                 Show).filter_by(id=show).first())
